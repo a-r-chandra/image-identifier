6673025f352fbf974c01a26643dff783d1ac7a65
a-r-chandra <ravi.developer.9@gmail.com>
2019-03-17 01:44:28 -0700

    cleanup

--- ImageProcessing/Program.cs
+++ ImageProcessing/Program.cs
@@ -18,105 +18,9 @@ namespace ImageProcessing
 
             var img1 = new Bitmap(@"C:\Users\archa\Documents\Projects\ImageProcessing\ImageProcessing\sample_a.tif");
 
-            var img1_r = ResizeImage(img1, img1.Width / 20, img1.Height / 20);
-
             var img2 = new Bitmap(@"C:\Users\archa\Documents\Projects\ImageProcessing\ImageProcessing\sample_a_resized.tif");
 
-                       
-            var wd = img1_r.Width;
-            var ht = img1_r.Height;
-
-            //Console.WriteLine($"w:{wd} h:{ht}");
-
-            var mtx = new Color[wd][];
-            for (var i=0; i<wd; i++) {
-
-                mtx[i] = new Color[ht];
-
-                for (var j=0; j<ht; j++) {
-
-                    var pixel = img1_r.GetPixel(i,j);
-                    //Console.WriteLine(pixel);
-                    //Console.WriteLine(pixel.GetHashCode());
-
-                    mtx[i][j] = pixel;
-                }
-            }
-
-
-            var same = true;
-
-            for (var i = 0; i < wd; i++)
-            {
-
-                for (var j = 0; j < ht; j++)
-                {
-
-                    var pixel = img2.GetPixel(i, j);
-                    //Console.WriteLine(pixel);
-                    //Console.WriteLine(pixel.GetHashCode());
-
-                    //Console.WriteLine($"{mtx[i][j].GetHashCode()} {pixel.GetHashCode()}");
-                    if (mtx[i][j].GetHashCode() != pixel.GetHashCode()) {
-                        same = false;
-                        break;
-                    }
-                }
-            }
-
-
-            if (same)
-            {
-                Console.WriteLine("The images are same");
-            }
-            else
-            {
-                Console.WriteLine("Not same");
-            }
-
-
-
-
-
-
-
-
-            //Console.WriteLine($"mtx len:{mtx.Length}");
-            /*
-            for (var i=0; i<mtx.Length; i++) {
-
-                var m = mtx[i];
-
-                for (var j=0; j<m.Length; j++) {
-
-                    //Console.WriteLine(m[j]);
-
-                }
-                
-            }
-            */
-
-
-
-            /*
-              
-            if (img1 == img2)
-            {
-                Console.WriteLine("The images are same");
-            }
-            else {
-                Console.WriteLine("Not same");
-            }
-             
-             var img2 = new Bitmap(@"C:\Users\archa\Documents\Projects\ImageProcessing\ImageProcessing\sample_a_same.tif");
-
-            
-             */
-
-
-
-
-
+            AreSameImages(img1, img2);
 
 
             end();
@@ -124,7 +28,7 @@ namespace ImageProcessing
         }
 
 
-        public bool AreImagesSame(Bitmap image1, Bitmap image2) {
+        public static bool AreSameImages(Bitmap image1, Bitmap image2) {
 
             var img1W = image1.Width;
             var img1H = image1.Height;
@@ -133,20 +37,16 @@ namespace ImageProcessing
             var img2H = image2.Height;
 
             if (img1H > img2H && img1W > img2W) {
-
                 image1 = ResizeImage(image1, img2W, img2H);
-
             }else if (img1H < img2H && img1W < img2W) {
-
                 image2 = ResizeImage(image2, img1W, img1H);
             }
 
             var same = true;
 
-            for (var i = 0; i < img1W; i++)
+            for (var i = 0; i < image1.Width/4; i++)
             {
-
-                for (var j = 0; j < img1H; j++)
+                for (var j = 0; j < image1.Height/4; j++)
                 {
                     var img1Pixel = image1.GetPixel(i, j);
                     var img2Pixel = image2.GetPixel(i, j);
@@ -172,9 +72,7 @@ namespace ImageProcessing
             return same;
         }
 
-
-
-        public void CreateResizedImage() {
+        public static void CreateResizedImage() {
             var img = new Bitmap(@"C:\Users\archa\Documents\Projects\ImageProcessing\ImageProcessing\sample_a.tif");
 
             var img2 = ResizeImage(img, img.Width / 20, img.Height / 20);

7caa719106eb411a83533140e669a0bb9c1a831d
a-r-chandra <ravi.developer.9@gmail.com>
2019-03-17 13:15:08 -0700

    updates

--- ImageProcessing/Program.cs
+++ ImageProcessing/Program.cs
@@ -28,63 +28,138 @@ namespace ImageProcessing
         }
 
 
-        public static bool AreSameImages(Bitmap image1, Bitmap image2) {
 
-            if (image1.Width > image2.Width && image1.Height > image2.Height) {
+
+
+
+
+        //the method of resize matters. can change that but have to check timing
+        public static bool AreSameImages(Bitmap image1, Bitmap image2) {
+            var rezTimer = new Stopwatch();
+            rezTimer.Start();
+
+            if (image1.Width > image2.Width || image1.Height > image2.Height) {
+                Console.WriteLine("Resizing to image2 's dimensions");
                 image1 = ResizeImage(image1, image2.Width, image2.Height);
-            }else if (image1.Width < image2.Width && image1.Height < image2.Height)
+            }
+            else if(image1.Width < image2.Width || image1.Height < image2.Height)
             {
+                Console.WriteLine("Resizing to image1 's dimensions");
                 image2 = ResizeImage(image2, image1.Width, image1.Height);
             }
+            rezTimer.Stop();
+            Console.WriteLine($"Resize took {rezTimer.ElapsedMilliseconds}ms");
+
 
             var same = true;
-            var counter = 0;
+            var pixelsChecked = 0;
 
-            for (var i = 0; i < image1.Width / 4; i++)
+            var imgCompTimer = new Stopwatch();
+            imgCompTimer.Start();
+
+            for (var i = 0; i < image1.Width; i= i+image1.Width/3)
+            //for (var i = 0; i < image1.Width; i++)
             {
-                for (var j = 0; j < image1.Height / 4; j++)
+                for (var j = 0; j < image1.Height; j=j+image1.Height/3)
+                //for (var j = 0; j < image1.Height; j++)
                 {
+                    pixelsChecked++;
+
+                    if (image1.GetPixel(i, j).GetHashCode() != image2.GetPixel(i, j).GetHashCode())
+                    {
+                        same = false;
+                        break;
+                    }
+                   
+                }
+            }
+            imgCompTimer.Stop();
+            Console.WriteLine($"Image compare took {imgCompTimer.ElapsedMilliseconds}ms");
+
+
+            if (same)
+            {
+                Console.WriteLine("The images are same....OK");
+            }
+            else
+            {
+                Console.WriteLine("Images are not same X");
+            }
+
+            Console.WriteLine($"Pixels checked {pixelsChecked}");
+                       
+            return same;
+        }
+
+        public static bool AreSameImages2(Bitmap image1, Bitmap image2)
+        {
+            var rezTimer = new Stopwatch();
+            rezTimer.Start();
+
+            if (image1.Width > image2.Width || image1.Height > image2.Height)
+            {
+                Console.WriteLine("Resizing to image2 's dimensions");
+                image1 = new Bitmap(image1, new Size(image2.Width, image2.Height));
+            }
+            else if (image1.Width < image2.Width || image1.Height < image2.Height)
+            {
+                Console.WriteLine("Resizing to image1 's dimensions");
+                image2 = new Bitmap(image2, new Size(image1.Width, image1.Height));
+            }
+            rezTimer.Stop();
+            Console.WriteLine($"Resize took {rezTimer.ElapsedMilliseconds}ms");
+
+
+            var same = true;
+            var pixelsChecked = 0;
+
+            var imgCompTimer = new Stopwatch();
+            imgCompTimer.Start();
+
+            for (var i = 0; i < image1.Width; i = i + image1.Width / 3)
+            //for (var i = 0; i < image1.Width; i++)
+            {
+                for (var j = 0; j < image1.Height; j = j + image1.Height / 3)
+                //for (var j = 0; j < image1.Height; j++)
+                {
+                    pixelsChecked++;
+
                     if (image1.GetPixel(i, j).GetHashCode() != image2.GetPixel(i, j).GetHashCode())
                     {
                         same = false;
                         break;
                     }
 
-                    counter++;
                 }
             }
+            imgCompTimer.Stop();
+            Console.WriteLine($"Image compare took {imgCompTimer.ElapsedMilliseconds}ms");
 
 
             if (same)
             {
-                Console.WriteLine("The images are same");
+                Console.WriteLine("The images are same....OK");
             }
             else
             {
-                Console.WriteLine("Not same");
+                Console.WriteLine("Images are not same X");
             }
 
-            Console.WriteLine($"pixels checked {counter}");
-                       
+            Console.WriteLine($"Pixels checked {pixelsChecked}");
+
             return same;
         }
 
-        public static void CreateResizedImage() {
-            var img = new Bitmap(@"C:\Users\archa\Documents\Projects\ImageProcessing\ImageProcessing\sample_a.tif");
-
-            var img2 = ResizeImage(img, img.Width / 20, img.Height / 20);
-
-            img2.Save(@"C:\Users\archa\Documents\Projects\ImageProcessing\ImageProcessing\sample_a_resized.tif");
-        }
-
+        //lossless
         public static Bitmap ResizeImage(Image image, int width, int height)
         {
             //init();
+            Console.WriteLine("Resizing");
 
             var destRect = new Rectangle(0, 0, width, height);
             var destImage = new Bitmap(width, height);
 
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+            //destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
 
             using (var graphics = Graphics.FromImage(destImage))
             {
@@ -106,30 +181,11 @@ namespace ImageProcessing
             return destImage;
         }
 
+        //lossy
+        public static Bitmap ResizeImage2(Image image, int width, int height) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            return new Bitmap(image, width, height);
+        }
 
         private static void init()
         {
